71ae539b132383c0a9d9a2cb34f5f2c4be2c9b95
hwan098 <75323120+hwan098@users.noreply.github.com>
2023-06-19 16:40:26 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.11 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+import java.lang.StringBuffer;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        StringBuffer str = new StringBuffer(my_string);
+        answer = str.reverse().toString();
+        
+        return answer;
+    }
+}
