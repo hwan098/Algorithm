37b338f82eb607647b23484b4cf3cac678c6a800
hwan098 <75323120+hwan098@users.noreply.github.com>
2023-06-19 15:55:04 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,10 +1,8 @@
 class Solution {
     public int solution(int[] array, int height) {
         int answer = 0;
-        for(int i=0; i<array.length; i++){
-            if(array[i] > height){
-                answer++;
-            }
+        for(int i: array){
+            answer += (i > height) ? 1 : 0;
         }
         return answer;
     }
