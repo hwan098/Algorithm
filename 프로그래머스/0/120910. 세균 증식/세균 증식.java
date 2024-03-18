1f754f1dacb73657beaf158024c7c44b40f4237c
hwan <75323120+hwan098@users.noreply.github.com>
2024-03-18 21:43:35 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int t) {
+        
+        int answer = 0;
+     
+        for(int i = 0;  i < t; i++){ 
+            answer = n;
+            answer += n;
+            n *= 2;
+        }
+        
+        return answer;
+    }
+}
