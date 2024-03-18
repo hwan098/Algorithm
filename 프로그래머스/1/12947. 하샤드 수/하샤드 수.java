ad573736e94cb6cbba63ef3eeb8b88226894a71c
hwan <75323120+hwan098@users.noreply.github.com>
2024-03-18 22:54:43 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int n = x;
+
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return x % sum == 0 ? true : false;
+    
+    }
+}
